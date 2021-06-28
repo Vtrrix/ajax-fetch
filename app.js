bd382b31544c6bcb4be02725650d96564b99db04
Vivek <viveksonybcd@gmail.com>
2021-06-29 01:28:35 +0530

    added scroll pagination and error handling

--- app.js
+++ app.js
@@ -1,26 +1,75 @@
 const displayArea = document.getElementById("data");
 const callForData = document.getElementById("call");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+nextBtn.style.display = "none";
+prevBtn.style.display = "none";
 
-callForData.addEventListener("click", () => {
+const reqObj = new XMLHttpRequest();
+let i = 1,
+  dataSize;
+
+function sendReq() {
+  // for button navigation-----------------------------------
+  displayArea.innerHTML = "";
+  nextBtn.style.display = "initial";
+  prevBtn.style.display = "initial";
+  // -----------------------------------------------------------
   callForData.style.display = "none";
-
-  const reqObj = new XMLHttpRequest();
+  reqObj.open(
+    "GET",
+    `https://jsonplaceholder.typicode.com/posts?userId=${i}`,
+    true
+  );
   reqObj.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
+    if (this.readyState == 4) {
       const data = JSON.parse(this.responseText);
-      data.map((ele) => {
-        displayArea.innerHTML += ` <div class="element">
-        <h2>${ele.userId}</h2>
-        <h3>
-          ${ele.title}
-        </h3>
-        <p>
-          ${ele.body}
-        </p>
-      </div>`;
-      });
+      dataSize = data.length;
+      if (this.status == 200) {
+        data.map((ele) => {
+          displayArea.innerHTML += ` <div class="element">
+              <h3>${ele.userId}</h3>
+              <div>
+                <h4>
+                  ${ele.title}
+                </h4>
+                <p>
+                  ${ele.body}
+                </p>
+              </div>
+          </div>`;
+        });
+      } else {
+        console.log(this.status);
+        displayArea.innerHTML += `<h2> Oops! Something Went Wrong.</h2>`;
+      }
     }
   };
-  reqObj.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
+
   reqObj.send();
+
+  // for scroll navigation----------------------
+  // i++;
+  // window.onscroll = function () {
+  //   if (
+  //     window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+  //     i <= dataSize
+  //   ) {
+  //     sendReq();
+  //   }
+  // };
+  // -----------------------------------------
+}
+
+callForData.addEventListener("click", sendReq);
+
+// for button navigation-----------------------------------
+nextBtn.addEventListener("click", () => {
+  i++;
+  sendReq();
 });
+prevBtn.addEventListener("click", () => {
+  i--;
+  sendReq();
+});
+// -----------------------------------------------------------
